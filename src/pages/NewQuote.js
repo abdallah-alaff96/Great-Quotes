a881c9e3c75317ba9c40a91cfaf5b8309437d3e0
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-01-09 18:42:40 +0200

    adding useHistory object and use history.push to navigate /quotes

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,12 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQoute = () => {
+  const history = useHistory();
+
   const addQouteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQouteHandler} />;
