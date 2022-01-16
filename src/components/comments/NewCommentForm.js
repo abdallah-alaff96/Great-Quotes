8561cc58fb7640e84d397b6bfc6f2663d17cd12f
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-01-17 00:40:24 +0200

    VVVVVVIMP Adding the comment features with important logic Part 1 (the main part)

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,9 +1,15 @@
-import { useRef } from 'react';
-
-import classes from './NewCommentForm.module.css';
+import { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { addComment } from "../../lib/api";
+import useHttp from "../../hooks/use-http";
+import classes from "./NewCommentForm.module.css";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+  const { sendRequest, status } = useHttp(addComment, true);
+  const params = useParams();
+  const { quoteId } = params;
 
   const submitFormHandler = (event) => {
     event.preventDefault();
@@ -11,16 +17,23 @@ const NewCommentForm = (props) => {
     // optional: Could validate here
 
     // send comment to server
+    const enteredText = commentTextRef.current.value;
+    sendRequest({ commentData: { enteredText }, quoteId });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
-      <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+      {/* {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )} */}
+      <div className={classes.control}>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
--- src/lib/api.js
+++ src/lib/api.js
@@ -80,7 +80,6 @@ export async function getAllComments(quoteId) {
   const response = await fetch(`${FIREBASE_DOMAIN}/comments/${quoteId}.json`);
 
   const data = await response.json();
-
   if (!response.ok) {
     throw new Error(data.message || "Could not get comments.");
   }
@@ -95,6 +94,5 @@ export async function getAllComments(quoteId) {
 
     transformedComments.push(commentObj);
   }
-
   return transformedComments;
 }
